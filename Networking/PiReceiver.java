b2c21fce544bff7ef109e8a28b045813b869e830
pitifulnick <pitifulnick@gmail.com>
2013-02-24 20:24:07 +0000

    update PiReceiver

--- Networking/PiReceiver.java
+++ Networking/PiReceiver.java
@@ -1,14 +1,20 @@
 import org.zeromq.ZMQ;
 
-public class PiReceiver {
-
-	public static void main(String[] args) {
-		ZMQ.Context context = ZMQ.context(1);
-		ZMQ.Socket receiver = context.socket(ZMQ.PULL);
+public class PiReceiver extends Thread{
+	private ZMQ.Context context;
+	private ZMQ.Socket receiver;
+	
+	public PiReceiver() {
+		context = ZMQ.context(1);
+		receiver = context.socket(ZMQ.PULL);
 		receiver.connect("tcp://192.168.1.100:10000");
-		String str = new String(receiver.recv(0)).trim();
-		System.out.println("received");
-		System.out.println(str);
+	}
+	
+	public byte[] getNextMsg(){
+		return receiver.recv(0);
+	}
+	
+	public void terminate(){
 		receiver.close();
 		context.term();
 	}
