de5193dc4a13809cb9952aee411c16b1d4217339
epithatree <epithatree@gmail.com>
2013-03-06 18:03:09 +0000

    Intialized interface with the parser.

---
+++ Controller/src/controller/ParserMessage.java
@@ -0,0 +1,11 @@
+package controller;
+
+
+public class ParserMessage {
+
+	public Byte[] message;
+	public Integer orderID;
+	public Integer symbol;
+	
+	
+}
---
+++ Controller/src/controller/ParserReader.java
@@ -0,0 +1,7 @@
+package controller;
+
+public interface ParserReader {
+
+	public ParserMessage getMessage();
+	
+}
