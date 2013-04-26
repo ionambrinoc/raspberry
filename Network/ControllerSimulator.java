60997e7477af7c3854b2cce41a63faefa1c103d2
pitifulnick <pitifulnick@gmail.com>
2013-04-26 16:55:44 +0100

    better simulator

--- Network/ControllerSimulator.java
+++ Network/ControllerSimulator.java
@@ -3,10 +3,37 @@ package networking;
 public class ControllerSimulator{
 	private static ControllerNetwork network;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		network = new ControllerNetwork();
+		String worker = null;
 		while(true){
-			network.send("hello".getBytes(), "192.168.1.100");
+			if(network.hasMessage()){
+				byte[] msg = network.nextMessage();
+				byte[] id;
+				switch (msg[0]){
+					case 0:
+						id = new byte[msg.length-1];
+						System.arraycopy(msg, 1, id, 0, msg.length-1);
+						worker = new String(id);
+						System.out.println(worker+" is up");
+						break;
+					case 1:
+						id = new byte[msg.length-1];
+						System.arraycopy(msg, 1, id, 0, msg.length-1);
+						System.out.println(new String(id)+" is down");
+						worker = null;
+						break;
+					case 2:
+						System.out.println("Confirmation received");
+						break;
+				}
+			}
+			if(worker == null){
+				System.out.println("waiting for worker");
+				Thread.sleep(3000);
+			}else{
+				network.send("hello".getBytes(), worker);
+			}
 		}
 	}
 }
--- Network/PiSimulator.java
+++ Network/PiSimulator.java
@@ -3,8 +3,11 @@ public class PiSimulator extends Thread{
 	public static void main(String[] args) throws InterruptedException {
 		PiNetwork network = new PiNetwork();
 		
+		int i = 0;
 		while(true){
+			i = (i+1) % 5;
 			network.sendToVisualization(network.recv());
+			if(i == 0) network.sendToToController("ok".getBytes());
 		}
 	}
 }
