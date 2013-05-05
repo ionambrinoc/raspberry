7c83ef44b1d74c9e04618edcbec54cf78b47726f
pitifulnick <pitifulnick@gmail.com>
2013-05-05 16:55:08 +0100

    final controller

---
+++ newController/Controller/Controller.java
@@ -0,0 +1,66 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import Network.ControllerNetwork;
+
+public class Controller {
+
+	static HashMap<Integer,String> symbolToId;// maps symbol to a Pi ID
+	static HashMap<String,Integer> idToSize = new HashMap<String,Integer>();// maps PiID to number of symbols sent to that Pi
+	
+	public static void main(String[] args) {
+		
+		ControllerNetwork network = new ControllerNetwork();
+		ArrayList<String> workers = new ArrayList<>();
+		MessageProcessor msgProcessor = new MessageProcessor(workers,idToSize);
+		LinkedBlockingQueue<Message> MessageQueue = new LinkedBlockingQueue<Message>(1000);
+		symbolToId= new HashMap<Integer,String>();
+		Parser parser = new Parser(MessageQueue);
+		parser.start();
+		
+		while(true){
+			if(network.hasMessage()){
+				byte[] msg = network.nextMessage();
+				msgProcessor.processMsg(msg);
+			}
+			if(workers.isEmpty()){
+				System.out.println("waiting for worker");
+				byte[] msg = network.nextMessage();
+				msgProcessor.processMsg(msg);
+			}else{
+					try {
+						Message msg = MessageQueue.take();
+						int symbol = msg.symbol;
+						byte[] byteMsg = msg.toBytes();
+//						if(msg.symbol == 6553)
+						network.send(byteMsg, chooseWorker(symbol,workers));
+					} catch (InterruptedException e) {
+						System.out.println("Failed to take message");
+						e.printStackTrace();
+					}
+			}
+		}
+	}
+
+	//This method finds the Pi that deals with the symbol
+	private static String chooseWorker(int symbol, ArrayList<String> workers) {
+		if ((!symbolToId.containsKey(symbol)) || (symbolToId.containsKey(symbol) && !workers.contains((symbolToId.get(symbol))))){
+			String minWorker = workers.get(0);
+			int minSize = idToSize.get(minWorker);
+			for (String worker: workers){
+				int size = idToSize.get(worker);
+				if (size<minSize){
+					minSize = size;
+					minWorker = worker;
+				}
+			}
+			symbolToId.put(symbol, minWorker);
+			idToSize.put(minWorker, minSize+1);
+		}
+		return symbolToId.get(symbol);
+	}
+
+}
---
+++ newController/Network/ControllerNetwork.java
@@ -0,0 +1,70 @@
+package Network;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+/**
+ * Create an instance of this class
+ * And use only this class to send and recv msg
+ * Call send(msg, identity) to send a msg to a specific worker
+ * Call hasMessage() to check if there is an msg received
+ * Call nextMessage() to recv a msg in bytes
+ * 
+ * @author yuchen
+ *
+ */
+
+public class ControllerNetwork {
+	private ZContext context;
+	private ControllerVentilator ventilator;
+	private ZMQ.Socket controller;
+	private ZMQ.Poller poller;
+	
+	public ControllerNetwork() {
+		context = new ZContext();
+		controller = context.createSocket(ZMQ.PAIR);
+		controller.bind("inproc://tocontroller");
+		ventilator = new ControllerVentilator(context);
+		ventilator.start();
+		
+		poller = new ZMQ.Poller(1);
+		poller.register(controller, ZMQ.Poller.POLLIN);
+	}
+	/**
+	 * send a msg to a particular worker
+	 * 
+	 * @param msg		The message you want to send to a worker
+	 * @param identity	The identity of a worker
+	 */
+	public void send(byte[] msg, String identity){
+		controller.sendMore(identity);
+		controller.sendMore("");
+		controller.send(msg, 0);
+	}
+	/**
+	 * use this method to check is there a message coming
+	 * Call this method before call has Message, so that you won't be blocked
+	 * 
+	 * @return	True if there is at least one message, False if there isn't any
+	 */
+	public boolean hasMessage(){
+		poller.poll(0);
+		return poller.pollin(0);
+	}
+	
+	/**
+	 * Call this method to receive a message
+	 * This method can block if there isn't any message coming,
+	 * so call hasMessage() before call nextMessage
+	 * 
+	 * Message format:
+	 * [1][ID]			for a worker is up
+	 * [2][ID]			for a worker is down
+	 * [3][MSG][ID]		for MSG sent by a worker
+	 * 
+	 * @return	The message in bytes:
+	 */
+	public byte[] nextMessage(){
+		return controller.recv();
+	}
+}
